c1eecbe35223f04e8ac50842fbab99065411ecaf
Kailous <kailous@live.cn>
2022-07-05 02:10:54 +0800

    ceshi2

--- pages/updown-button/[namespace]/[id].js
+++ pages/updown-button/[namespace]/[id].js
@@ -4,6 +4,18 @@ import { useRouter } from "next/router";
 export default function Button() {
   const router = useRouter();
   const { namespace, id } = router.query;
-
+  const defaultTheme = {
+    colors: {
+      primary: '#38ff00',
+      secondary: '#38ff00',
+      background: 'transparent',
+      text: '#292929',
+      highlight: '#e095ed',
+      icon: '#292929',
+    },
+    fonts: {
+      body: 'inherit',
+    },
+  };
   return <UpdownButton namespace={namespace} id={id} />;
 }
