805542121f4bd21cfba226728c3bbed5975a2abe
Kailous <kailous@live.cn>
2022-07-05 03:40:37 +0800

    ceshi

--- pages/like-button/[namespace]/[id].js
+++ pages/like-button/[namespace]/[id].js
@@ -1,30 +1,10 @@
-import { LikeButton } from '@lyket/react';
+import { LikeButton } from "@lyket/react";
+import { useRouter } from "next/router";
 
-export Faq = () => {
-  return (
-      <>
-        <h4 id="Do you like pizza?">Do you like pizza?</h4>
-        <LikeButton
-            id="do-you-like-pizza"
-            namespace="faq"
-            hideCounterIfLessThan={1}
-        >
-          {({
-              handlePress,
-              totalLikes,
-              userLiked,
-              isLoading,
-              isCounterVisible
-            }) => (
-              <>
-                <button onClick={handlePress} disabled={isLoading}>
-                  🍕
-                </button>
-                {isCounterVisible && <div>Total: {totalLikes}</div>}
-                {userLiked && <div>Great! I like pizza as well!</div>}
-              </>
-          )}
-        </LikeButton>
-      </>
-  )
+export default function Button() {
+  const router = useRouter();
+  const { namespace, id } = router.query;
+
+  return <h4>hello!</h4>;<LikeButton namespace={namespace} id={id} />;
+
 }
