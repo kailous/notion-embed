a81d857b5722b0299e65dbd328409d49d8a79b1a
Kailous <kailous@live.cn>
2022-07-05 03:28:16 +0800

    ceshi

--- pages/like-button/[namespace]/[id].js
+++ pages/like-button/[namespace]/[id].js
@@ -2,29 +2,29 @@ import { LikeButton } from '@lyket/react';
 
 export Faq = () => {
   return (
-    <>
-      <h4 id="Do you like this page">喜欢这篇帖子吗？</h4>
-      <LikeButton
-        id="do-you-like-this-page"
-        namespace="faq"
-        hideCounterIfLessThan={1}
-      >
-        {({
-          handlePress,
-          totalLikes,
-          userLiked,
-          isLoading,
-          isCounterVisible
-        }) => (
-          <>
-            <button onClick={handlePress} disabled={isLoading}>
-              🍕
-            </button>
-            {isCounterVisible && <div>Total: {totalLikes}</div>}
-            {userLiked && <div>好棒! 谢谢你的点赞!</div>}
-          </>
-        )}
-      </LikeButton>
-    </>
+      <>
+        <h4 id="Do you like pizza?">Do you like pizza?</h4>
+        <LikeButton
+            id="do-you-like-pizza"
+            namespace="faq"
+            hideCounterIfLessThan={1}
+        >
+          {({
+              handlePress,
+              totalLikes,
+              userLiked,
+              isLoading,
+              isCounterVisible
+            }) => (
+              <>
+                <button onClick={handlePress} disabled={isLoading}>
+                  🍕
+                </button>
+                {isCounterVisible && <div>Total: {totalLikes}</div>}
+                {userLiked && <div>Great! I like pizza as well!</div>}
+              </>
+          )}
+        </LikeButton>
+      </>
   )
 };
