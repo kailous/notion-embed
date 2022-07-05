aa378afdf35148e8432ba68655d77af0d35935cb
kailous <kailous@live.cn>
2022-07-05 15:11:23 +0800

    ceshi22

--- pages/like-button/[namespace]/[id].js
+++ pages/like-button/[namespace]/[id].js
@@ -7,8 +7,8 @@ export default function Button() {
 
   return <div id="likethis">
     <div id="title">
-      <h4>你喜欢这篇文章</h4>
-      <h4>{namespace} <span>|</span> {id} 吗?</h4>
+      <h4>你喜欢这篇文章吗</h4>
+      <h4>{namespace} <span>|</span> {id}</h4>
     </div>
     <LikeButton
         id={id}
